332e7e80589504ea53f13701c17f8de2c8ad3527
Felipe Trost <felipe.trost@gmail.com>
2020-08-12 20:48:45 -0400

    Implemented cache in currency command

--- commands/currency_exchange_rate.js
+++ commands/currency_exchange_rate.js
@@ -1,6 +1,9 @@
 // Get exchange rate for two different currencies
 const axios = require("axios");
-const endpoint = "https://free.currconv.com/api/v7/convert"
+const endpoint = "https://free.currconv.com/api/v7/convert";
+
+const Cache = require("../utils/cache");
+const cache = new Cache(1000*60*30);
 
 module.exports = async (msg, params) => {
     const currencies = params.toUpperCase().split(" ");
@@ -10,21 +13,29 @@ module.exports = async (msg, params) => {
 
     const [from, to] = currencies;
 
-    try {
-        const response = await axios.get(endpoint, {
-            params: {
-                q: `${from}_${to}`,
-                compact: "ultra",
-                apiKey: process.env.exchange_rate_api_key
-            }
-        })
+    const cachedData = cache.get(currencies.join(" "));
+    if(cachedData){
+        msg.reply(cachedData);
+    }else{
+        try {
+            const response = await axios.get(endpoint, {
+                params: {
+                    q: `${from}_${to}`,
+                    compact: "ultra",
+                    apiKey: process.env.exchange_rate_api_key
+                }
+            })
 
-        const exchangeRate = response.data[`${from}_${to}`]
+            const exchangeRate = response.data[`${from}_${to}`];
+            const reply = `The exchange rate is ${exchangeRate}`;
 
-        msg.reply(`The exchange rate is ${exchangeRate}`);
+            // Cache response
+            cache.cache(currencies.join(" "), reply);
 
-    } catch (error) {
-        console.error("Error with currency command", error)
-        msg.reply("Perdon, intenta mas tarde");
+            msg.reply(reply);
+        } catch (error) {
+            console.error("Error with currency command", error)
+            msg.reply("Perdon, intenta mas tarde");
+        }
     }
 }
